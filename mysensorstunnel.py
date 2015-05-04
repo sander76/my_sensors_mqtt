45f484ed4ddd706aa158b2eb65c44a9403e57d73
sander <s.teunissen@gmail.com>
2015-05-04 13:32:05 +0200

    fine tuning

--- mysensorstunnel.py
+++ mysensorstunnel.py
@@ -1,4 +1,6 @@
 import json
+import socket
+import sys
 
 __author__ = 'sander'
 import serial
@@ -11,16 +13,22 @@ lgr = logging.getLogger(__name__)
 
 
 
-
 class Tunneler:
-    def __init__(self,translation_table, address='test.mosquitto.org', port=1883, serial_connection=fake_serial.Serial()):
+    def __init__(self,
+                 address='test.mosquitto.org',
+                 port=1883,
+                 serial_connection=fake_serial.Serial(),
+                 update_host='',
+                 update_port=8889):
         # the table with mysensors to mqtt topics.
-        self.translation_table=translation_table
+        self.translation_table
+        self.translation_file="mysensors_mqtt_table.json"
+        self.load_translation_table()
 
         # the mqtt client.
         self.mqtt = mqtt_client.Client()
         self.mqtt.on_message = self.on_message_from_mqtt
-        self.mqtt.connect(address,port=port)
+        self.mqtt.connect(address, port=port)
         self.mqtt.subscribe('mysensors/#')
         self.mqtt.loop_start()
 
@@ -30,20 +38,63 @@ class Tunneler:
         t = threading.Thread(target=self.reader)
         t.start()
 
-    def get_mqtt_topic(self,node):
+        u_t = threading.Thread(target=self.update_translation_table,args=(update_host,update_port))
+        u_t.start()
+
+    def load_translation_table(self):
+        with open(self.translation_file) as fl:
+            self.translation_table=json.load(fl)
+
+    def save_translation_table(self,js):
+        with open(self.translation_file,"w") as fl:
+            json.dump(js,fl)
+
+    def update_translation_table(self,update_host,update_port):
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
         try:
-            topic = next(i[0]for i in self.translation_table if i[1]==node )
+            s.bind((update_host,update_port))
+        except socket.error,msg:
+            lgr.error("Bind failed. {} {}".format(msg[0],msg[1]))
+
+            #sys.exit()
+
+        s.listen(1)
+        lgr.debug("socket now listening")
+        conn,add = s.accept()
+        try:
+            while 1:
+                lgr.debug("waiting for incoming data.")
+                data=conn.recv(1024)
+                try:
+                    lgr.debug(data)
+                    js = json.loads(data)
+                    self.translation_table=js
+                    self.save_translation_table(js)
+                except ValueError,msg:
+                    lgr.debug(msg.message)
+
+        finally:
+            print ("closing connection")
+            conn.close()
+
+    def get_mqtt_topic(self, node):
+        try:
+            topic = next(i[0] for i in self.translation_table if i[1] == node)
             return topic
         except StopIteration:
             raise UserWarning("No mqtt topic found for : {}".format(node))
+        except TypeError:
+            raise UserWarning("No translation table available.")
 
-    def get_mysensor_node(self,topic):
+    def get_mysensor_node(self, topic):
         try:
-            node = next(i[1] for i in self.translation_table if i[0]==topic)
+            node = next(i[1] for i in self.translation_table if i[0] == topic)
             return node
         except StopIteration:
             raise UserWarning("no mysensor node found for : {}".format(topic))
-
+        except TypeError:
+            raise UserWarning("No translation table available.")
 
     def reader(self):
         while 1:
@@ -52,30 +103,28 @@ class Tunneler:
             bfr.append(x)
             while x:
                 x = self.serial_connection.read()
-                if x =='\n':
+                if x == '\n':
                     self.to_mqtt(''.join(bfr))
-                    bfr=[]
+                    bfr = []
                 else:
                     bfr.append(x)
 
-    def to_mqtt(self,data):
+    def to_mqtt(self, data):
         lgr.debug("incoming data from serial port: {}".format(data))
         try:
+            #todo : finish this.
             pass
-        except UserWarning,e:
+        except UserWarning, e:
             lgr.debug(e.message)
 
-    def on_message_from_mqtt(self,client, userdata, msg):
+    def on_message_from_mqtt(self, client, userdata, msg):
         lgr.debug("incoming data from mqtt: topic: {} payload: {}".format(msg.topic, msg.payload))
         try:
-            self.serial_connection.write("{}{}\n".format(self.get_mysensor_node(msg.topic),msg.payload))
-        except UserWarning,e:
+            self.serial_connection.write("{}{}\n".format(self.get_mysensor_node(msg.topic), msg.payload))
+        except UserWarning, e:
             lgr.debug(e.message)
 
 
-if __name__=="__main__":
-    with open('mysensors_mqtt_table.json') as fl:
-        js = json.load(fl)
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    #lgr.setLevel(logging.DEBUG)
-    Tunneler(js)
+    Tunneler()
---
+++ socket_test.py
@@ -0,0 +1,25 @@
+__author__ = 'sander'
+import socket
+import sys
+
+
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+try:
+    s.bind((HOST,PORT))
+except socket.error,msg:
+    print "Bind failed. {} {}".format(msg[0],msg[1])
+    sys.exit()
+
+s.listen(1)
+print ("socket now listening")
+conn,add = s.accept()
+try:
+    while 1:
+        print("waiting for incoming data.")
+        data=conn.recv(1024)
+        print (data)
+finally:
+    print ("closing connection")
+    conn.close()
